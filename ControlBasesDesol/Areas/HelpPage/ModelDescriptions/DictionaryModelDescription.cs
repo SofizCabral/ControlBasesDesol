257c4f17e5a2c6647174935d9417ce4a141a262b
SofizCabral <49485164+SofizCabral@users.noreply.github.com>
2021-03-24 19:52:43 -0300

    Agregar archivos de proyecto.

---
+++ ControlBasesDesol/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ControlBasesDesol.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ ControlBasesDesol/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ControlBasesDesol.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
