71252b2075da589b71bde19d42226ad4b4d3aa16
Emmanuel Pompolo <emmanuel.pompolo@fligoo.com>
2021-03-29 08:53:44 -0300

    add HomeController

---
+++ ControlBasesDesol/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ControlBasesDesol.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
